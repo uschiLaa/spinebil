2b9fd4b5b0104971931f818011e2db901ea07d65
manmita <das.manmita12@gmail.com>
2025-03-19 00:02:31 +0530

    bug fixes:multiple fixes to pass CRAN checks

--- R/timer.R
+++ R/timer.R
@@ -11,25 +11,42 @@
 #' @return numeric vector containing all distances
 #' @export
 #' @examples \donttest{
+#' library(purrr)  # Ensure purrr is loaded
 #' d <- spiralData(4, 1000)
-#' t <- purrr::rerun(10, tourr::basis_random(4))
+#' t <- map(1:10, tourr::basis_random(4))
 #' idx <- scagIndex("Skinny")
 #' timeSequence(d, t, idx, 10)
 #' }
 timeSequence <- function(d, t, idx, pmax){
   i <- 1
   dfTimer <- data.frame(t= numeric(), i=numeric())
+  if (is.null(d) || !is.numeric(as.matrix(d))) {
+    stop("Error: 'd' must be a numeric matrix.")
+  }
+  d <- as.matrix(d)
   for(pMatrix in t){
     if(i>pmax) break
+    if (is.null(pMatrix) || !is.numeric(as.matrix(pMatrix))) {
+      warning("Skipping iteration: 'pMatrix' is NULL or not numeric.")
+      next
+    }
+    pMatrix <- as.matrix(pMatrix)
     tictoc::tic.clearlog()
     tictoc::tic() #start timer
     dProj <- d %*% pMatrix
     res <- idx(dProj)
     tictoc::toc(log=TRUE,quiet=TRUE)
-    resT <- unlist(tictoc::tic.log(format=FALSE))["toc.elapsed"] -
-      unlist(tictoc::tic.log(format=FALSE))["tic.elapsed"]
+    log_values <- tictoc::tic.log(format = FALSE)
+    
+    if (length(log_values) > 0 && is.list(log_values[[1]])) {
+      resT <- log_values[[1]]$toc - log_values[[1]]$tic
+    } else {
+      resT <- NA  # Assign NA if no valid timing data
+      warning("tic.log() did not return expected values.")
+    }
+    
     dfTimer <- tibble::add_row(dfTimer, t=resT, i=i)
-    i <- i+1
+    i <- i + 1
   }
   return(dfTimer)
 }
